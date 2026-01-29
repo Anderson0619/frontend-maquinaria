255fb0c185ed63e756c5a1c59faca805319e791b
ELchucho1999. <ameramurillo869@gmail.com>
2026-01-29 15:02:15 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,34 +1,32 @@
 const withImages = require("next-images");
-const withPlugins = require("next-compose-plugins");
 const nextTranslate = require("next-translate");
-const withPWA = require("next-pwa");
 
-const { parsed: myEnv } = require("dotenv").config({
-  path: ".env",
-});
-
-const config = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 🔴 IMPRESCINDIBLE: Esto falta en tu archivo
+  output: 'standalone',
+  distDir: '.next',
+  
   images: {
     domains: ["storage.googleapis.com", "cdn.ndrz.io"],
   },
-  pwa: {
-    dest: "public",
-    maximumFileSizeToCacheInBytes: 5242880,
-    disable: process.env.NODE_ENV === "development",
-  },
-  typeScript: {
+  
+  // 🔴 CORREGIR: "typeScript" debe ser "typescript" (minúscula)
+  typescript: {
     ignoreBuildErrors: true,
   },
+  
   eslint: {
     ignoreDuringBuilds: true,
   },
 
   reactStrictMode: true,
-  webpack: (config, { webpack }) => {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-    config.experiments = { topLevelAwait: true, layers: true };
+  
+  webpack: (config) => {
+    config.experiments = { topLevelAwait: true };
     return config;
   },
 };
 
-module.exports = withPlugins([withImages, withPWA, nextTranslate], config);
+// 🔴 ELIMINAR: withPlugins, withPWA, dotenv
+module.exports = withImages(nextTranslate(nextConfig));
